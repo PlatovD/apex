67b2799b13b6751fd58a6b4deb4ddce3650e3eeb
Svyatoslav Sorokin <shadsoroka@yandex.ru>
2026-01-15 00:02:02 +0300

    Исправлены афинки

--- src/main/java/com/apex/storage/transformation/TransformationController.java
+++ src/main/java/com/apex/storage/transformation/TransformationController.java
@@ -3,15 +3,13 @@ package com.apex.storage.transformation;
 import com.apex.affine.AffineBuilder;
 import com.apex.affine.transformations.Axis;
 import com.apex.math.Matrix4x4;
+import com.apex.math.Vector3f;
 import com.apex.model.scene.RenderObject;
 import com.apex.reflection.AutoCreation;
 import com.apex.reflection.AutoInject;
 import com.apex.storage.CameraStorage;
 import com.apex.storage.SceneStorage;
 import javax.vecmath.Matrix4d;
-import javax.vecmath.Vector3f;
-import java.util.HashMap;
-import java.util.Map;
 
 @AutoCreation
 public class TransformationController {
@@ -21,53 +19,73 @@ public class TransformationController {
     @AutoInject
     private CameraStorage cameraStorage;
 
-    // Кэш трансформаций для каждого объекта — чтобы хранить position, rotation, scale
-    private final Map<RenderObject, TransformState> transformStateMap = new HashMap<>();
+    public void updateWorldMatrixForObject(RenderObject ro,
+                                           float scaleX, float scaleY, float scaleZ,
+                                           float rotationX, float rotationY, float rotationZ,
+                                           float positionX, float positionY, float positionZ) {
 
-    // Получить состояние трансформации для объекта
-    private TransformState getState(RenderObject ro) {
-        return transformStateMap.computeIfAbsent(ro, k -> new TransformState());
-    }
-
-    public void updateWorldMatrixForObject(RenderObject ro) {
-        TransformState state = getState(ro);
-        AffineBuilder builder = new AffineBuilder();
-
-        // Масштаб
-        builder.scale(state.scale.x, state.scale.y, state.scale.z);
-
-        // Поворот — через кватернионы (XYZ порядок)
-        builder.rotateQuat(Axis.X, (float) Math.toRadians(state.rotation.x));
-        builder.rotateQuat(Axis.Y, (float) Math.toRadians(state.rotation.y));
-        builder.rotateQuat(Axis.Z, (float) Math.toRadians(state.rotation.z));
-
-        // Перемещение
-        builder.translate(state.position.x, state.position.y, state.position.z);
-
-        // Собираем матрицу и конвертируем Matrix4d → Matrix4x4
-        Matrix4d affineMatrix = builder.build().getMatrix();
-        ro.setWorldMatrix(toMatrix4x4(affineMatrix));
+        Matrix4x4 worldMatrix = createAffineMatrix(
+                new Vector3f(positionX, positionY, positionZ),
+                new Vector3f(rotationX, rotationY, rotationZ),
+                new Vector3f(scaleX, scaleY, scaleZ)
+        );
+        ro.setWorldMatrix(worldMatrix);
     }
 
     public void updateWorldMatrixForActiveObjects(
             float scaleX, float scaleY, float scaleZ,
             float rotationX, float rotationY, float rotationZ,
-            float transitionX, float transitionY, float transitionZ
-    ) {
+            float positionX, float positionY, float positionZ) {
+
+        Matrix4x4 worldMatrix = createAffineMatrix(
+                new Vector3f(positionX, positionY, positionZ),
+                new Vector3f(rotationX, rotationY, rotationZ),
+                new Vector3f(scaleX, scaleY, scaleZ)
+        );
+
         for (RenderObject ro : sceneStorage.getActiveRenderObjects()) {
-            TransformState state = getState(ro);
-            state.position.set(transitionX, transitionY, transitionZ);
-            state.rotation.set(rotationX, rotationY, rotationZ);
-            state.scale.set(scaleX, scaleY, scaleZ);
-            updateWorldMatrixForObject(ro);
+            ro.setWorldMatrix(worldMatrix.copy());
         }
     }
 
-    public void moveCameraOnVector(javax.vecmath.Vector3f moveVector) {
+    public void updateWorldMatrixForObject(String objectName,
+                                           float scaleX, float scaleY, float scaleZ,
+                                           float rotationX, float rotationY, float rotationZ,
+                                           float positionX, float positionY, float positionZ) {
+
+        RenderObject ro = sceneStorage.getRenderObject(objectName);
+        if (ro != null) {
+            updateWorldMatrixForObject(ro,
+                    scaleX, scaleY, scaleZ,
+                    rotationX, rotationY, rotationZ,
+                    positionX, positionY, positionZ
+            );
+        }
+    }
+
+    private Matrix4x4 createAffineMatrix(Vector3f position,
+                                         Vector3f rotationDegrees,
+                                         Vector3f scale) {
+
+        AffineBuilder builder = new AffineBuilder();
+
+        builder.scale(scale.getX(), scale.getY(), scale.getZ());
+
+        builder.rotateQuat(Axis.X, (float) Math.toRadians(rotationDegrees.getX()));
+        builder.rotateQuat(Axis.Y, (float) Math.toRadians(rotationDegrees.getY()));
+        builder.rotateQuat(Axis.Z, (float) Math.toRadians(rotationDegrees.getZ()));
+
+        builder.translate(position.getX(), position.getY(), position.getZ());
+
+        Matrix4d affineMatrix = builder.build().getMatrix();
+        return toMatrix4x4(affineMatrix);
+    }
+
+    public void moveCameraOnVector(Vector3f moveVector) {
         // TODO: Позже
     }
 
-    // Костыль для конвертации Matrix4d → Matrix4x4
+    // Костыль для преобразования из Matrix4d в Matrix4x4
     private Matrix4x4 toMatrix4x4(Matrix4d m) {
         float[][] matrix = new float[4][4];
         for (int i = 0; i < 4; i++) {
@@ -77,11 +95,5 @@ public class TransformationController {
             matrix[i][3] = (float) m.getElement(i, 3);
         }
         return new Matrix4x4(matrix);
-    }
-
-    private static class TransformState {
-        Vector3f position = new Vector3f(0, 0, 0);
-        Vector3f rotation = new Vector3f(0, 0, 0); // в градусах
-        Vector3f scale = new Vector3f(1, 1, 1);
     }
 }
