1273c42825ed5cbe4d9b16c42e2c7a70d18f373b
Svyatoslav Sorokin <shadsoroka@yandex.ru>
2026-01-15 17:11:37 +0300

    Исправлен lookAt и ZBuffer

--- src/main/java/com/apex/model/scene/ZBuffer.java
+++ src/main/java/com/apex/model/scene/ZBuffer.java
@@ -19,18 +19,25 @@ public class ZBuffer {
     }
 
     public boolean setPixel(int x, int y, double zCord) {
-        if (zCord < 0)
-            return false;
         int bufferIndex = y * width + x;
-        if (bufferIndex < 0 || bufferIndex >= width * height)
+        if (bufferIndex < 0 || bufferIndex >= width * height) {
             return false;
-        if (buffer[bufferIndex] < zCord)
+        }
+
+        double normalizedZ = zCord;
+        if (normalizedZ < -1.0 || normalizedZ > 1.0) {
             return false;
-        buffer[bufferIndex] = zCord;
-        return true;
+        }
+        normalizedZ = (normalizedZ + 1.0) / 2.0; // [-1,1] → [0,1]
+
+        if (normalizedZ < buffer[bufferIndex]) {
+            buffer[bufferIndex] = normalizedZ;
+            return true;
+        }
+        return false;
     }
 
     public void clear() {
-        Arrays.fill(buffer, Float.MAX_VALUE);
+        Arrays.fill(buffer, Double.MAX_VALUE);
     }
 }
