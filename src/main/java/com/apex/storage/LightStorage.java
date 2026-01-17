a7fd4ff53418d912bcf75ffbc8de28197f260335
Svyatoslav Sorokin <shadsoroka@yandex.ru>
2026-01-17 12:07:42 +0300

    Добавлен класс Light

--- src/main/java/com/apex/storage/LightStorage.java
+++ src/main/java/com/apex/storage/LightStorage.java
@@ -6,6 +6,7 @@ import com.apex.math.Vector3f;
 import com.apex.reflection.AutoCreation;
 import com.apex.reflection.AutoInject;
 import com.apex.util.ActiveLightWrapper;
+import com.apex.tool.light.Light;
 
 import java.util.HashMap;
 import java.util.List;
@@ -14,17 +15,17 @@ import java.util.Objects;
 
 @AutoCreation
 public class LightStorage {
-    private final Map<String, Vector3f> lights = new HashMap<>();
+    private final Map<String, Light> lights = new HashMap<>();
     private String activeLight = Constants.DEFAULT_LIGHT_NAME;
 
     @AutoInject
     private ActiveLightWrapper activeLightWrapper;
 
     public LightStorage() {
-        addLight(Constants.DEFAULT_LIGHT_NAME, Constants.DEFAULT_LIGHT);
+        addLight(Constants.DEFAULT_LIGHT_NAME, new Light(Constants.DEFAULT_LIGHT, 0xFFFFFFFF));
     }
 
-    public void addLight(String name, Vector3f light) {
+    public void addLight(String name, Light light) {
         if (Objects.isNull(light))
             throw new LightStorageException("Light can't be null");
         if (Objects.isNull(name))
@@ -32,19 +33,34 @@ public class LightStorage {
         if (lights.containsKey(name))
             throw new LightStorageException("Duplicated light name. Unable to create a light. Change the name");
 
-        lights.put(name, light.copy());
+        lights.put(name, new Light(light.getPosition(), light.getColor()));
     }
 
-    public void updateLightColor(String name, Vector3f newColor) {
+    public void updateLightColor(String name, int newColor) {
         if (!lights.containsKey(name))
             throw new LightStorageException("Light not found: " + name);
-        if (Objects.isNull(newColor))
-            throw new LightStorageException("Light color can't be null");
 
-        lights.put(name, newColor.copy());
+        Light light = lights.get(name);
+        Light updated = new Light(light.getPosition(), newColor);
+        lights.put(name, updated);
 
         if (name.equals(activeLight)) {
-            activeLightWrapper.setActiveLight(newColor.copy());
+            activeLightWrapper.setActiveLight(updated.getPosition().copy());
+        }
+    }
+
+    public void updateLightPosition(String name, Vector3f newPosition) {
+        if (!lights.containsKey(name))
+            throw new LightStorageException("Light not found: " + name);
+        if (Objects.isNull(newPosition))
+            throw new LightStorageException("Position can't be null");
+
+        Light light = lights.get(name);
+        Light updated = new Light(newPosition, light.getColor());
+        lights.put(name, updated);
+
+        if (name.equals(activeLight)) {
+            activeLightWrapper.setActiveLight(newPosition.copy());
         }
     }
 
@@ -70,7 +86,7 @@ public class LightStorage {
         if (!lights.containsKey(name))
             return;
         activeLight = name;
-        activeLightWrapper.setActiveLight(lights.get(name).copy());
+        activeLightWrapper.setActiveLight(lights.get(name).getPosition().copy());
     }
 
     public Vector3f getActiveLight() {
@@ -83,13 +99,11 @@ public class LightStorage {
         return activeLight;
     }
 
-    public Vector3f getLight(String name) {
+    public Light getLight(String name) {
         if (!lights.containsKey(name))
             throw new LightStorageException("Light not found: " + name);
-        return lights.get(name).copy();
+        Light light = lights.get(name);
+        return new Light(light.getPosition(), light.getColor());
     }
 
-    public void setLightColor(String name, float r, float g, float b) {
-        updateLightColor(name, new Vector3f(r, g, b));
-    }
 }
---
+++ src/main/java/com/apex/tool/light/Light.java
@@ -0,0 +1,47 @@
+package com.apex.tool.light;
+
+import com.apex.math.Vector3f;
+
+public class Light {
+    private Vector3f position;
+    private int color;
+
+    public Light(Vector3f position, int color) {
+        this.position = position.copy();
+        this.color = color;
+    }
+
+    public Vector3f getPosition() {
+        return position.copy();
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setPosition(Vector3f position) {
+        this.position = position.copy();
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Light light = (Light) o;
+        return color == light.color && position.equals(light.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return position.hashCode() * 31 + Integer.hashCode(color);
+    }
+
+    @Override
+    public String toString() {
+        return "Light{position=" + position + ", color=" + color + '}';
+    }
+}
